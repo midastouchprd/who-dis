719fd8d48784cecd06316d2b8625344cae7d9224
Oluwatosin Awofeso <midastouchproductions@gmail.com>
2021-08-31 20:04:31 -0500

    use dynamic port

--- server/server.js
+++ server/server.js
@@ -25,6 +25,6 @@ app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Successfully Connected to Who Dis Api");
 });
